9d433df613ca7f75039b6aeab997305a5275827f
Kavindu Theekshana <kavindutheekshana@gmail.com>
2025-03-31 21:50:13 +0100

    Assets

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('/');
+// Route::get('about', [HomeController::class, 'about'])->name('about');
+// Route::get('contact', [HomeController::class, 'contact'])->name('contact');
 
 Route::middleware([
     'auth:sanctum',
